9d226be9adaf6d9141ade9dc1739842e76e1a7af
Sergio <kurisergio777777@gmail.com>
2022-03-22 18:43:40 -0600

    commit tree ee0e0f9de51ab079c1da54d472c8b479d4067b96

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+return string.toUpperCase();
+};
+
+function whisper(string){
+return string.toLowerCase();
+
+};
+
+const string = "hello"
+function logShout(string) {
+    console.log(string.toUpperCase()); 
+    };
+
+    let spy = "HELLO"
+    function logWhisper(spy) {
+    console.log(spy.toLowerCase());
+    
+    }
+
+let answer = "Lets have dinner together";
+
+    function sayHiToHeadphonedRoommate(string) {
+
+        if (string === string.toLowerCase()){
+            console.log("I can\'t hear you!")
+
+        }
+        if (string === string.toUpperCase()){
+            console.log("YES INDEED")
+
+        }
+        if (string = "Let's have dinner together!"){
+            console.log ("I would love to!")
+
+        }
+
+        }
+
+       
+
+
+   
